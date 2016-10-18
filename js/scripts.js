4e9587149b98f6de17b1e55cad891e6123feb417
Matt Eichsteadt and Skye Atwood <matt@>
2016-10-18 16:17:52 -0700

    perfected the game. it is awesome. i am enthusiastic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,34 @@
+var countDog = 0;
+var countCat = 0;
+var bang = "Bang!";
 $('img.cat').click(function(){
-  $('ul.cat').append('<li>Bang!!!</li>');
+  $('ul.cat').append('<li>'+ bang+ '</li>');
+  $('#middle').children('h1').remove();
+  countCat++;
+  bang+="!";
+  if(countCat === 5) {
+    $('#middle').append('<h1>The cat wins!!!</h1>');
+    $('ul.cat').children().remove();
+    $('ul.dog').children().remove();
+    countCat=0;
+    countDog=0;
+    bang = "Bang!";
+  };
 });
 
 $('img.dog').click(function(){
-  $('ul.dog').append('<li>Bang!!!</li>');
+  $('ul.dog').append('<li>'+ bang+ '</li>');
+  $('#middle').children('h1').remove();
+  countDog++;
+  bang+="!";
+  if(countDog === 5) {
+    $('#middle').append('<h1>The dog wins!!!</h1>');
+    $('ul.dog').children().remove();
+    $('ul.cat').children().remove();
+    countDog=0;
+    countCat=0;
+    bang = "Bang!";
+  };
 });
 
 $('#secret-corgi').one('click', function(){
